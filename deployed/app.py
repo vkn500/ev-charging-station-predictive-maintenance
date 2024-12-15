e194c40c630deaf2e8a77eecc3a0929954a09929
Tarun Kumar Sahu <88510766+badflametarun@users.noreply.github.com>
2024-12-16 02:58:12 +0530

    Update app.py

--- deployed/app.py
+++ deployed/app.py
@@ -10,7 +10,7 @@ st.title('EV Charging Station Maintenance Prediction')
 # Load the trained model
 @st.cache(allow_output_mutation=True)
 def load_model():
-    model = joblib.load('model.pkl')
+    model = joblib.load('/model.pkl')
     return model
 
 model = load_model()
