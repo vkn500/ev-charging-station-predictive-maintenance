5f5f4648ef48dd7eb9e36a5894f5cfca70e79307
Tarun Kumar Sahu <88510766+badflametarun@users.noreply.github.com>
2025-01-28 07:03:10 +0000

    Add "Developed by" section in Streamlit app sidebar to credit contributors

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,7 +71,9 @@ def main():
     st.subheader("About")
     st.write("This project aims to enhance the reliability and efficiency of EV charging stations by predicting their maintenance needs. By leveraging machine learning techniques, the system can forecast potential faults and schedule maintenance proactively, thereby minimizing downtime and ensuring a seamless charging experience for users.")
 
-
+    #developed by section
+    st.subheader("Developed by")
+    st.write("Komendra Dhruvey, Shiv Kumar Sao, Tarun Sahu and Vishal Nishad")
 
 if __name__ == '__main__':
     main()
